a65f8f435745b2cd8d673e53076c9d35f98de5dc
Patrick <patrick.yrkim@gmail.com>
2020-09-27 01:17:10 -0700

    Add apollo connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './utils/apolloClient';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <ApolloProvider cliet={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
